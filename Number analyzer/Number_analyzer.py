e588c11ccc7298495bdb5ccd503ce1808b5c2e76
Lexbrazilev <burtyt@mail.ru>
2024-04-02 19:06:20 +0300

    Добавьте файлы проекта.

---
+++ Number analyzer/Number_analyzer.py
@@ -0,0 +1,7 @@
+﻿number = int(input("Введите пятизначное целое число "))
+n1 = number // 10000
+n2 = number // 1000 % 10
+n3 = number // 100 % 10
+n4 = number // 10 % 10
+n5 = number % 10
+print((n4 ** n5) * n3 / (n1 - n2))
